6286f96d5da38578f3cd6e7864d006c1af076176
austinbrovick <austinbrovick@gmail.com>
2016-05-02 18:58:43 -0700

    commit with tag_form.html

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.core.urlresolvers import reverse
 
 from organizer.models import Startup, Tag  # importing tables
 
@@ -27,6 +28,7 @@ class Post(models.Model):
 
 
     def get_absolute_url(self):
+        print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
         return reverse('blog_post_detail', kwargs={'year':self.pub_date.year, 'month': self.pub_date.month, 'slug': self.slug})
 
 # posts have many tags, tags can be on many posts
--- organizer/forms.py
+++ organizer/forms.py
@@ -1,28 +1,47 @@
 from django import forms
-from .models import Tag
+from .models import Tag, Startup, NewsLink
 from django.core.exceptions import ValidationError
 
 
 
-class TagForm(forms.Form):
-    name = forms.CharField(max_length=31)
-    slug = forms.SlugField(max_length=31, help_text='A label for URL config')
-
-
-    def save(self):
-        new_tag = Tag.objects.create(name=self.cleaned_data['name'], slug=self.cleaned_data['slug'])
-        return new_tag
-
-
-    def clean_name(self):
-        return self.cleaned_data['name'].lower()
-
-
+class SlugCleanMixin:
+    # mixin class for slug cleaning method
     def clean_slug(self):
         new_slug = (self.cleaned_data['slug'].lower())
         if new_slug == 'create':
             raise ValidationError('Slug man not be "create".')
         return new_slug
+
+
+class TagForm(SlugCleanMixin, forms.ModelForm):
+    class Meta:
+        model = Tag
+        fields = '__all__' # fields = ['name', 'slug']
+
+    def save(self):
+        new_tag = Tag.objects.create(name=self.cleaned_data['name'], slug=self.cleaned_data['slug'])
+        return new_tag
+
+    def clean_name(self):
+        return self.cleaned_data['name'].lower()
+
+class NewsLinkForm(forms.ModelForm):
+    class Meta:
+        model = NewsLink
+        fields = '__all__'
+
+class StartupForm(SlugCleanMixin, forms.ModelForm):
+    class Meta:
+        model = Startup
+        fields = '__all__'
+
+
+
+
+
+    # name = forms.CharField(max_length=31)
+    # slug = forms.SlugField(max_length=31, help_text='A label for URL config')
+
 
 
 # model fields vs form fields
